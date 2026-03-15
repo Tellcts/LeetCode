5edf5cc7a094ef40d87120baa10eb59dc06808e1
tzcan <tzcan@foxmail.com>
2026-03-15 23:22:24 +0800

    fix file:102.h

--- solutions/102.h
+++ solutions/102.h
@@ -1,6 +1,7 @@
 // 二叉树的层序遍历
 #pragma once
 
+#include <queue>
 #include <utility>
 #include <vector>
 
@@ -14,7 +15,8 @@ struct TreeNode {
 
 class Solution {
  public:
-  std::vector<std::vector<int>> levelOrder(TreeNode* __root) {
+  // 两个数组
+  std::vector<std::vector<int>> levelOrder_V1(TreeNode* __root) {
     if (__root == nullptr) {
       return {};
     }
@@ -29,6 +31,28 @@ class Solution {
         if (node->right) nxt.push_back(node->right);
       }
       cur = std::move(nxt);
+      ans.push_back(std::move(vals));
+    }
+    return ans;
+  }
+
+  // 一个队列
+  std::vector<std::vector<int>> levelOrder_V2(TreeNode* __root) {
+    if (__root == nullptr) {
+      return {};
+    }
+    std::vector<std::vector<int>> ans;
+    std::queue<TreeNode*> q;
+    q.push(__root);
+    while (!q.empty()) {
+      std::vector<int> vals;
+      for (int n = q.size(); n--;) {
+        TreeNode* node = q.front();
+        q.pop();
+        vals.push_back(node->val);
+        if (node->left) q.push(node->left);
+        if (node->right) q.push(node->right);
+      }
       ans.push_back(std::move(vals));
     }
     return ans;
